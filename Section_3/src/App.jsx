099a941893f390373a9afdbef2378c206013ba96
Balaji B <50548656+balajibabu03@users.noreply.github.com>
2024-01-07 23:59:43 +0530

    Section 3 completed - checkin
    This is the final checkin for section 3.

--- Section_3/src/App.jsx
+++ Section_3/src/App.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcepts";
-import { CORE_CONCEPTS } from "./data";
 import TabButton from "./components/Header/TabButton";
+import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleClick(selected){
+    setSelectedTopic(selected)
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a topic.</p>
+  if(selectedTopic){
+    tabContent = 
+      <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
+  }
   return (
     <div>
       <Header></Header>
@@ -12,20 +33,27 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts!</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} ></CoreConcept>
+            {
+              CORE_CONCEPTS.map((conceptsItem)=>(
+                <CoreConcept key={conceptsItem.title} {...conceptsItem}/>
+              ))
+            
+            //here below items are created based on data by looping
+            /* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} ></CoreConcept>
             <CoreConcept {...CORE_CONCEPTS[1]} ></CoreConcept>
             <CoreConcept {...CORE_CONCEPTS[2]} ></CoreConcept>
-            <CoreConcept {...CORE_CONCEPTS[3]} ></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[3]} ></CoreConcept> */}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton isSelected={selectedTopic=== "components"} onSelect={() => handleClick("components")} >Components</TabButton>
+            <TabButton isSelected={selectedTopic=== "jsx"} onSelect={() => handleClick("jsx")} >JSX</TabButton>
+            <TabButton isSelected={selectedTopic=== "props"} onSelect={() => handleClick("props")} >Props</TabButton>
+            <TabButton isSelected={selectedTopic=== "state"} onSelect={() => handleClick("state")} >State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
--- Section_3/src/components/Header/TabButton.jsx
+++ Section_3/src/components/Header/TabButton.jsx
@@ -1,6 +1,9 @@
 // Eventhough we don't have any props in component, It has a children props by default.
-export default function TabButton({children}){
+export default function TabButton({children, onSelect, isSelected}){
+    
     return(
-        <li><button>{children}</button></li>
+        <li>
+            <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
+        </li>
     )
 }
